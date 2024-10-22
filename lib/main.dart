101309d3d7302f9fc037377974aa2a111914c04d
FernandoMachadoo <igino.machado@gmail.com>
2024-10-22 09:54:49 -0300

    segunda aula

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,24 @@ import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:todo_app/pages/home/home_page.dart';
 import 'package:todo_app/providers/task_group_provider.dart';
+import 'package:todo_app/providers/task_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: 'https://pclxmgtpkfoibmegugtd.supabase.co',
+    url: 'https://uvmifoxitryrbapzjhqn.supabase.co',
     anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBjbHhtZ3Rwa2ZvaWJtZWd1Z3RkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjcxMDM2NjUsImV4cCI6MjA0MjY3OTY2NX0.cxtdnGrNS8ve89F3ZL60VV5CvvA9WrV6piRfuaOUIcU',
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2bWlmb3hpdHJ5cmJhcHpqaHFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjgzODg3NjAsImV4cCI6MjA0Mzk2NDc2MH0.Biw-5iVFIKBuu_qdA06ozfdc1D3oCkzwJ0dU_oX5NLQ',
   );
 
   runApp(MultiProvider(
     providers: [
       ChangeNotifierProvider(
         create: (_) => TaskGroupProvider()..listTaskGroups(),
+      ),
+      ChangeNotifierProvider(
+        create: (_) => TaskProvider(),
       ),
     ],
     child: const MyApp(),
--- lib/repository/supabase_repository.dart
+++ lib/repository/supabase_repository.dart
@@ -1,5 +1,6 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:todo_app/models/task_group.dart';
+import 'package:todo_app/models/task_model.dart';
 
 class SupabaseRepository {
   Future<List<TaskGroup>> listTaskGroups() async {
@@ -7,4 +8,12 @@ class SupabaseRepository {
     final response = await supabase.from('task_groups').select();
     return response.map((task) => TaskGroup.fromMap(task)).toList();
   }
+
+  Future<List<Task>> listTasksByGroup(String groupId) async {
+    final supabase = Supabase.instance.client;
+    final response = await supabase.from('tasks').select().eq('task_group_id', groupId);
+    return response.map((task) => Task.fromMap(task)).toList();
+  }
+
+
 }
